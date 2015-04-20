81c35eee73e893ebcc0e6e2dadbfc6349a4769d1
asaph <ace@dontletgo.com>
2015-04-20 10:19:52 -0700

    - upgrade tomcat7 to version 7.0.61 - change all apache urls to https - use sha256 checksums instead of sha1 for apache resources

--- Formula/tomcat7.rb
+++ Formula/tomcat7.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Tomcat7 < Formula
-  homepage "http://tomcat.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz"
-  sha1 "c9f8d87a212091c33027f122aba90b78eb1c0a01"
+  homepage "https://tomcat.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.61/bin/apache-tomcat-7.0.61.tar.gz"
+  sha256 "2528ad7434e44ab1198b5692d5f831ac605051129119fd81a00d4c75abe1c0e0"
 
   option "with-ssl", "Configure SSL and generate a self-signed cert. If building with APR,\n\tuse OpenSSL to generate the cert, otherwise use java's keytool"
   option "with-apr", "Use Apache Portable Runtime"
@@ -22,9 +22,9 @@ class Tomcat7 < Formula
   depends_on 'tomcat-native' => ['--without-tomcat', '--with-apr'] if build.with? 'apr'
 
   resource 'fulldocs' do
-    url "http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59-fulldocs.tar.gz"
-    version "7.0.59"
-    sha1 "96852de4949a1765b1a5478c8c586220bfc16377"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.61/bin/apache-tomcat-7.0.61-fulldocs.tar.gz"
+    version "7.0.61"
+    sha256 "30f0ccc080a46dd7fc2dfaccd3f3b8efdec5d5d42e235637e1de518a77ea86f3"
   end
 
   resource 'mysql-connector' do
