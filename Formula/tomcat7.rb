84c4b15a33ef3cd2c99980f81f79f52838097963
asaph <ace@dontletgo.com>
2015-08-26 14:38:03 -0700

    Upgrade tomcat7 to version 7.0.64

--- Formula/tomcat7.rb
+++ Formula/tomcat7.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tomcat7 < Formula
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.63/bin/apache-tomcat-7.0.63.tar.gz"
-  sha256 "b5d878a17de2421a078d8907583076b507e67dbf1567c6f4346d70c88473f8ad"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.64/bin/apache-tomcat-7.0.64.tar.gz"
+  sha256 "76c729037f9fd6a0f44fa41c147c4c7291e68f74b79ec474c0bcef8a57661306"
 
   option "with-ssl", "Configure SSL and generate a self-signed cert. If building with APR,\n\tuse OpenSSL to generate the cert, otherwise use java's keytool"
   option "with-apr", "Use Apache Portable Runtime"
@@ -22,9 +22,9 @@ class Tomcat7 < Formula
   depends_on 'tomcat-native' => ['--without-tomcat', '--with-apr'] if build.with? 'apr'
 
   resource 'fulldocs' do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.63/bin/apache-tomcat-7.0.63-fulldocs.tar.gz"
-    version "7.0.63"
-    sha256 "a86abb866733565a054adaf02ccfb77fdea08d71c17978e1c1bc942090f63353"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.64/bin/apache-tomcat-7.0.64-fulldocs.tar.gz"
+    version "7.0.64"
+    sha256 "72ee358e6da7fc02772ae4a6d78a24290d4693f30294c76557639c4c5c29b8a5"
   end
 
   resource 'mysql-connector' do
